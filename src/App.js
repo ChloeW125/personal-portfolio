439f8219c6e0713f7a42c1de2957c75e73a0fbab
Chloe Wei <wei.chloe55@gmail.com>
2025-06-21 22:29:47 -0400

    navbar added and styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/index.js'
+import Layout from './components/layout'
 
 function App() {
   return (
