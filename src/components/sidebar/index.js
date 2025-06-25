5e64fdd443954791151701b3eadd56633673c9bf
Chloe Wei <wei.chloe55@gmail.com>
2025-06-24 22:42:08 -0400

    navbar features added

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -2,7 +2,8 @@ import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import headerImage from '../../assets/images/header-icon.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser, faLink } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
     return (
@@ -21,6 +22,18 @@ const Sidebar = () => {
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/chloe-wei-84b14a265/">
+                    <FontAwesomeIcon icon={faLink} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://github.com/ChloeW125">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>)
 }
 
