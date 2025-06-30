43e94b56c7e738f6d1f0cedbc85fafdce68144ea
Chloe Wei <wei.chloe55@gmail.com>
2025-06-29 22:33:55 -0400

    Updated about page

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -24,8 +24,37 @@ const About = () => {
                         strArray={"About Me".split("")}
                         idx={15}/>
                 </h1>
-                <p>Hi! I'm a ......</p>
-                <p>Draft intro description here :D</p>
+                <p>Hi! I'm Chloe, a second-year Computer Science student at the University of Waterloo. 
+                    Happy to have you land on my page—whether on purpose or by accident!</p>
+                <p>I'm passionate about building meaningful tech, with a strong interest in full-stack 
+                    software development, machine learning, and project management. Right now, I'm working on two projects:</p>
+                <ul>
+                    <li>
+                        An AI-powered plant doctor app that helps gardeners detect and treat plant diseases.
+                    </li>
+                    <li>
+                        A machine learning–powered medical insurance prediction app, designed to help 
+                        individuals estimate their fees and to serve as an opportunity for me to explore potential bias in ML algorithms and how to minimize it.
+                    </li>
+                </ul>
+                <p>I’m also working as a Systems Developer with Environment and Climate Change Canada, where I:</p>
+                <ul>
+                    <li>
+                        Organize task assignments for over 300 field technicians across Canada,
+                    </li>
+                    <li>
+                        Visualize hydrometric data to help partners make confident, data-driven predictions, and
+                    </li>
+                    <li>
+                        Explore building a RAG chatbot to support the Government of Canada and its partners in quickly accessing key hydrometric data information.
+                    </li>
+                </ul>
+                <p>
+                    When I'm not zealously developing software, I love gardening, long-distance running, reading (fantasy has my heart!), 
+                    diving into manhwas, watching anime and K-dramas, and convincing my brother that Brooklyn Nine-Nine is, in fact, better than The Rookie.
+                </p>
+                <p>I'm just getting started in the tech world and excited to explore new roles and opportunities as I grow within the community. Feel free 
+                    to reach out through the Contact page—I’d love to hear from you!</p>
             </div>
             <div className="stage-cube-container">
                 <div className="cubespinner">
